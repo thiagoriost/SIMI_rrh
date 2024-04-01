b7c5b79f7920bad55284028579a45417d7cd0762
thiagoriost <thiagoriostapia@gmail.com>
2024-04-01 09:37:57 -0500

    avance 8 abril

---
+++ simi-front-end/src/app/components/field-input-edit-text/field-input-edit-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-field-input-edit-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './field-input-edit-text.component.html',
+  styleUrl: './field-input-edit-text.component.scss'
+})
+export class FieldInputEditTextComponent {
+
+}
--- simi-front-end/src/app/modules/ideas/pages/new-idea-page/new-idea-page.component.ts
+++ simi-front-end/src/app/modules/ideas/pages/new-idea-page/new-idea-page.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -8,17 +8,51 @@ import { MyErrorStateMatcher } from '../../../../share/utils/complements';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Editor, NgxEditorModule, Toolbar } from 'ngx-editor';
+import jsonDoc from './doc';
 
 
 
 @Component({
   selector: 'app-new-idea-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatDividerModule, MatCheckboxModule],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatDividerModule, MatCheckboxModule,
+    NgxEditorModule],
   templateUrl: './new-idea-page.component.html',
   styleUrl: './new-idea-page.component.scss',
 })
-export class NewIdeaPageComponent {
+export class NewIdeaPageComponent implements OnInit, OnDestroy{
+
+
+  /**
+   * Propiedades para el campo editor menu
+   */
+  editor!: Editor;
+  html = '';
+  // editordoc = jsonDoc;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  ngOnDestroy(): void {
+    this.editor?.destroy();
+  }
+  // form = new FormGroup({
+  //   editorContent: new FormControl(
+  //     { value: jsonDoc, disabled: false },
+  //     // Validators.required()
+  //   ),
+  // });
 
   /**
    * Propiedades para la configuracipon del campo email
